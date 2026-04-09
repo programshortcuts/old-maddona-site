1ecd4848e0d6fcf28ba3e990398ad9c1864693ac
tt <tt@Mac.lan>
2026-04-09 02:44:48 -0400

    main

---
+++ js/visuals/carousel.js
@@ -0,0 +1,6 @@
+const swiper = new Swiper('.swiper-container', {
+    loop: true,            // continuous loop
+    autoplay: { delay: 2300 }, // auto-slide every 4 seconds
+    slidesPerView: 1,      // show 1 review at a time
+    speed: 600,             // smooth transition
+});
