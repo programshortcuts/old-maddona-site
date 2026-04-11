fb29467a09a475f1935b27f98739d565e94cc841
tt <tt@Mac.lan>
2026-04-11 15:49:54 -0400

    main

--- js/visuals/images.js
+++ js/visuals/images.js
@@ -2,26 +2,33 @@
 export function initImageHandling(){
     const sections = document.querySelectorAll('.section')
     sections.forEach(el => {
-        el.addEventListener('click', (e) => {
-            const section = e.target.closest('.section')
-            const img = section.querySelector('img')
-            if(img){
-                section.classList.toggle('checked')
-            }
-        });
+        el.addEventListener('click',handleImgColors);
         el.addEventListener('mouseout', (e) => {
-            const section = e.target.closest('.section')
-            const img = section.querySelector('img')
-            if(img){
-                if(section.classList.contains('checked')){}
-            }
+            
         });
     })
-    function removeAllChecks(sections){
+    function greyOut(){
         sections.forEach(el => {
-            if(el ){
-                
+            if(el.classList.contains('colored') ){
+                el.classList.remove('colored')
             }
         })    
+    }
+    function handleImgColors(e){
+        if(e.type == 'click'){
+            const section = e.target.closest('.section')
+            const img = section.querySelector('img')
+            
+            toggleColor(section)
+
+
+        }
+        
+
+    }
+    function toggleColor(el){
+        console.log(el)
+        greyOut()
+        el.classList.toggle('colored')
     }
 }
