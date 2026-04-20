23b883ea829d36c3d77f9b6c27fc3fa23073d396
tt <tt@Mac.lan>
2026-04-20 10:43:36 -0400

    main

--- js/ui/toggle-nav.js
+++ js/ui/toggle-nav.js
@@ -3,13 +3,13 @@
 export function initToggleNav() {
     const sideNavBtn = document.querySelector('#sideNavBtn')
     const imgSmoke = document.querySelector('#madonnaShilouetteLogo')
+    const pageWrapper = document.querySelector('.page-wrapper')
     sideNavBtn.addEventListener('keydown', (e) => {
         const key = e.key.toLowerCase()
         if(key == 'enter'){
-            const mobileHeaderNavUl = document.querySelector('.mobile-header-nav > ul') 
-            mobileHeaderNavUl.classList.toggle('expand')
-            imgSmoke.classList.toggle('smoke-img')
-            console.log(imgSmoke)
+            // const mobileHeaderNavUl = document.querySelector('.mobile-header-nav > ul') 
+            pageWrapper.classList.toggle('expand')
+            // imgSmoke.classList.toggle('smoke-img')
         }
     });
     sideNavBtn.addEventListener('click', (e) => {
